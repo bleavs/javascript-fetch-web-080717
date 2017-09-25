fb9dc66037e8a031da07d8301df3dc6f26131e8f
bleavs <bleavitt@colgate.edu>
2017-09-25 10:22:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 const app = "I don't do much.";
+
+// w/ xhr
+
+// let xhr = new XMLHttpRequest();
+// xhr.open('GET', 'https://api.github.com/repos/jquery/jquery/commits');
+// xhr.responseType = 'json';
+//
+// xhr.onload = function() {
+//   console.log(xhr.response);
+// };
+//
+// xhr.onerror = function() {
+//   console.log('Booo');
+// };
+//
+// xhr.send();
+
+//w/ fetch()
+
+fetch('https://api.github.com/repos/jquery/jquery/commits')
+
+  .then(res => res.json()) // getting resp. from fetch - using json to turn into json
+  .then(json => console.log(json)); // then passing json to console log
+
+
+
+  const token = 'ef6e0be756fe4f756a1170b2e49d0738ec18f43b'
+  fetch('https://api.github.com/user/repos', {
+    headers: {
+      Authorization: `token ${token}`
+    }
+    }).then(res => res.json()).then(json => console.log(json));
+    
